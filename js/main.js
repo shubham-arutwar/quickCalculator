2b823bdd550d22f6728cc203bb3ef8e182b5ee43
shubham-arutwar <shubhamarutwar23@gmail.com>
2021-12-08 23:16:09 +0530

    final functionality

--- js/main.js
+++ js/main.js
@@ -1,38 +1,34 @@
-var total = 0, val, x = true;
-var arr=[];
+var total = 0, val, x = true, arr=[];
+
+function sw() {
+    if (x == true) {
+        x = false;
+        display("#switch","Substraction");
+    } else {
+        x = true;
+        display("#switch","Addition");
+    }   
+}
 
 function calc(val) {
-    if (x == true) {
-        total = total + val;
-    } else {
-        total = total - val;
-    }
+    x ? total = total + val : total = total - val;
+
     (x == true) ? arr.push(`+ ${val}`) : arr.push(`- ${val}`); //arr=[`+ ${val}`,...arr]
-    display(total);
+    display("#result",total);
     history();
+}
+
+function display(divId,num) {
+    document.querySelector(divId).innerHTML = num;
+}
+
+function history(num) {
+    document.querySelector("#history").innerHTML = arr.join(" ");
 }
 
 function clr() {
     total = 0;
     arr = [];
-    display(total);
+    display("#result",total);
     history();
-}
-
-function sw() {
-    if (x == true) {
-        x = false;
-    } else {
-        x = true;
-    }
-    display(x)    
-}
-
-function display(num) {
-    document.querySelector("#result").innerHTML = num;
-}
-
-function history(num) {
-    document.querySelector("#history").innerHTML = arr;
-    console.log(arr);
 }
